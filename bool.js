e0513417eea07422abae6e886fbc3144df7aa413
admin <you@example.com>
2021-12-16 14:39:34 +0100

    calcme

---
+++ bool.js
@@ -0,0 +1,16 @@
+let iLikeYou =true;
+
+
+if (iLikeYou){
+    document.write('You like me' );
+}else {
+    document.write('you dont like me!!!');
+}
+
+let myNumber = 10;
+
+if(myNumber > 9) {
+    document.write( ' its great');
+}else {
+    document.write('its not good');
+}
---
+++ switch.js
@@ -0,0 +1,12 @@
+/* const country = 'Germany'
+let currency
+
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+} */
+
